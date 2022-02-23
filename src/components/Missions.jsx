a40a60b7d11a0cb6524914ebfde55f80832290f7
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-02-23 10:49:32 -0300

    requisitos completados

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 export default class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map(({ name, year, country, destination }, i) => (
+          <MissionCard
+            key={ name + i }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />))}
       </div>
     );
   }
